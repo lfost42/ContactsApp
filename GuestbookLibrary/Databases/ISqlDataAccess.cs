0d6c0944424f9856fec7e0efe37a84c6238183ce
Lynda Foster <lfost42@wgu.edu>
2022-02-26 10:31:31 -0800

    added class library

---
+++ GuestbookLibrary/Databases/ISqlDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace GuestbookLibrary.Databases
+{
+    public interface ISqlDataAccess
+    {
+        List<T> LoadData<T, U>(string sqlStatement,
+                               U parameters,
+                               string connectionStringName,
+                               bool isStoredProcedure = false);
+
+        void SavaData<T>(string sqlStatement,
+                         T parameters,
+                         string connectionStringName,
+                         bool isStoredProcedure = false);
+    }
+}
---
+++ GuestbookLibrary/Models/Guests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuestbookLibrary.Models
+{
+    public class Guests
+    {
+        [Required]
+        [Display(Name = "First")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DataType(DataType.Html)]
+        [Display(Name = "LinkedIn")]
+        public string Linkedin { get; set; }
+        [DataType(DataType.Html)]
+        public string Website { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date => DateTime.Now;
+
+        [MaxLength(255)]
+        public string Message { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Name")]
+        public string FullName => $"{LastName}, {FirstName}";
+
+        [NotMapped]
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+        public byte[] ImageData { get; set; }
+        public string ImageType { get; set; }
+        public int Id { get; set; }
+    }
+}
